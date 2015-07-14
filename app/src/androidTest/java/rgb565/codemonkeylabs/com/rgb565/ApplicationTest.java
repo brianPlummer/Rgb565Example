c0d604d8e345afbc5796032eafa1866aa3c5d3f3
Brian Plummer <brian.plummer@nytimes.com>
2015-07-14 11:56:24 -0400

    initial stub

---
+++ app/src/androidTest/java/rgb565/codemonkeylabs/com/rgb565/ApplicationTest.java
@@ -0,0 +1,13 @@
+package rgb565.codemonkeylabs.com.rgb565;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
